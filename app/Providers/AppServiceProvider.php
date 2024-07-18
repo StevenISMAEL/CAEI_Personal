ab9382e102eec977f961226e2e2b355f3134d35b
Jordan <jspuruncajasc@unt.edu>
2024-07-17 21:59:27 -0500

    provider update ✅

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider {
@@ -17,6 +18,10 @@ class AppServiceProvider extends ServiceProvider {
      * Bootstrap any application services.
      */
     public function boot(): void {
+        if (config("app.env") === "production") {
+            URL::forceScheme("https");
+        }
+
         if (Auth::check()) {
             session(["last_activity" => time()]);
         }
